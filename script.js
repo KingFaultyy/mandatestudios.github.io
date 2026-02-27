c4e7bff10f096a9bb1d72b8018a85d7652c06e2f
KingFaultyy <t.e.parks08@gmail.com>
2026-02-26 23:03:09 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-// Set update date here
+// Update Date (CHANGE THIS)
 const updateDate = new Date("March 15, 2026 18:00:00").getTime();
-
 const countdown = document.getElementById("countdown");
 
 const interval = setInterval(function() {
 
     const now = new Date().getTime();
     const distance = updateDate - now;
+
+    if (distance < 0) {
+        clearInterval(interval);
+        countdown.innerHTML = "Update Released";
+        document.querySelector(".countdown-box").style.background = "lime";
+        return;
+    }
 
     const days = Math.floor(distance / (1000 * 60 * 60 * 24));
     const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -17,11 +23,15 @@ const interval = setInterval(function() {
         days + "d " +
         hours + "h " +
         minutes + "m " +
-        seconds + "s ";
-
-    if (distance < 0) {
-        clearInterval(interval);
-        countdown.innerHTML = "Update Released";
-    }
+        seconds + "s";
 
 }, 1000);
+
+// Modal Functions
+function openModal() {
+    document.getElementById("patchModal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("patchModal").style.display = "none";
+}
